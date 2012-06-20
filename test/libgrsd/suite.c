ab0711d9c53951ffce41c67c2d0397f596130759
Robin Doer <robin@robind.de>
2012-06-20 22:44:59 +0200

    libgrsd_test links against libcheck and provides an initial Suite.

--- test/libgrsd/suite.c
+++ test/libgrsd/suite.c
@@ -1,3 +1,19 @@
+#include <check.h>
+
+Suite* libgrsd_suite() {
+  Suite* s = suite_create("libgrsd_test");
+  return s;
+}
+
 int main(int argc, char** argv) {
-  return 0;
+  int nfailed;
+
+  Suite* s = libgrsd_suite();
+  SRunner* sr = srunner_create(s);
+  srunner_run_all(sr, CK_NORMAL);
+
+  nfailed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+
+  return (nfailed == 0) ? 0 : 1;
 }
