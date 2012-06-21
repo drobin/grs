194140b9cde5b0a56639ca759de63fdb33e2c99a
Robin Doer <robin@robind.de>
2012-06-21 21:09:32 +0200

    Skeleton for libssh_proxy
    This module is used to emulate a libssh for the testsuite.

---
+++ test/libgrsd/libssh_proxy.c
@@ -0,0 +1,10 @@
+#include <libssh/libssh.h>
+#include <libssh/server.h>
+
+void libssh_proxy_init() {
+
+}
+
+void libssh_proxy_destroy() {
+
+}
---
+++ test/libgrsd/libssh_proxy.h
@@ -0,0 +1,7 @@
+#ifndef LIBSSH_PROXY_H
+#define LIBSSH_PROXY_H
+
+void libssh_proxy_init();
+void libssh_proxy_destroy();
+
+#endif /* LIBSSH_PROXY */
