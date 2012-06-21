888d0dfe93eeb4384222badf0413c86741c2fece
Robin Doer <robin@robind.de>
2012-06-21 20:41:44 +0200

    Test for grsd_init() and grsd_destroy()

--- test/libgrsd/suite.c
+++ test/libgrsd/suite.c
@@ -1,7 +1,27 @@
 #include <check.h>
 
+#include <grsd.h>
+
+START_TEST(init_destroy) {
+  grsd_t handle;
+  int result;
+
+  handle = grsd_init();
+  fail_unless(handle != NULL);
+
+  result = grsd_destroy(handle);
+  fail_unless(result == 0);
+}
+END_TEST
+
 Suite* libgrsd_suite() {
   Suite* s = suite_create("libgrsd_test");
+
+  TCase* tc = tcase_create("libgrsd");
+  suite_add_tcase(s, tc);
+
+  tcase_add_test(tc, init_destroy);
+
   return s;
 }
 
