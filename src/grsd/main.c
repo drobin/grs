fe2257ad07b163fa6e4ba38032bb57274cbff09e
Robin Doer <robin@robind.de>
2012-06-22 19:18:26 +0200

    grsd-tool can setup port and hostkey

--- src/grsd/main.c
+++ src/grsd/main.c
@@ -1,6 +1,8 @@
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <grsd.h>
 
@@ -10,8 +12,14 @@ static void leave_handler(int sig) {
   grsd_listen_exit(handle);
 }
 
+static void usage() {
+  printf("USAGE\n");
+  exit(1);
+}
+
 int main(int argc, char** argv) {
   struct sigaction sa;
+  int ch;
 
   memset(&sa, 0, sizeof(struct sigaction));
   sa.sa_handler = leave_handler;
@@ -19,6 +27,19 @@ int main(int argc, char** argv) {
   sigaction(SIGINT, &sa, NULL);
 
   handle = grsd_init();
+
+  while ((ch = getopt(argc, argv, "p:k:?")) != -1) {
+    switch (ch) {
+    case 'p':
+      grsd_set_listen_port(handle, atoi(optarg));
+      break;
+    case 'k':
+      grsd_set_hostkey(handle, optarg);
+      break;
+    default:
+      usage();
+    }
+  }
 
   grsd_listen(handle);
   grsd_destroy(handle);
