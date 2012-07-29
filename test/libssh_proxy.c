0e8d73ff672efb472c0452d65c216dfc2c7037ac
Robin Doer <robin@robind.de>
2012-07-29 12:04:28 +0200

    libssh_proxy: A framework to manipulate the behaviour of the libssh-implementation.

---
+++ test/libssh_proxy.c
@@ -0,0 +1,114 @@
+#include <sys/queue.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "libssh_proxy.h"
+
+struct libssh_proxy_option {
+  const char* option;
+  int value;
+  LIST_ENTRY(libssh_proxy_option) entries;
+};
+
+LIST_HEAD(_libssh_proxy_option_head, libssh_proxy_option);
+
+struct libssh_proxy_func {
+  const char* func;
+  struct _libssh_proxy_option_head options_head;
+  LIST_ENTRY(libssh_proxy_func) entries;
+};
+
+LIST_HEAD(_libssh_proxy_head, libssh_proxy_func);
+
+static struct libssh_proxy_func* find_func(const char* func, int create) {
+  struct libssh_proxy_func* entry;
+
+  LIST_FOREACH(entry, libssh_proxy_env, entries) {
+    if (strcmp(entry->func, func) == 0) {
+      return entry;
+    }
+  }
+
+  if (create) {
+    entry = malloc(sizeof(struct libssh_proxy_func));
+    entry->func = func;
+    LIST_INIT(&entry->options_head);
+    LIST_INSERT_HEAD(libssh_proxy_env, entry, entries);
+  } else {
+    entry = NULL;
+  }
+
+  return entry;
+}
+
+static struct libssh_proxy_option* find_option(struct libssh_proxy_func* func,
+                                               const char* option, int create) {
+  struct libssh_proxy_option* entry;
+
+  LIST_FOREACH(entry, &func->options_head, entries) {
+    if (strcmp(entry->option, option) == 0) {
+      return entry;
+    }
+  }
+
+  if (create) {
+    entry = malloc(sizeof(struct libssh_proxy_option));
+    entry->option = option;
+    LIST_INSERT_HEAD(&func->options_head, entry, entries);
+  } else {
+    entry = NULL;
+  }
+
+  return entry;
+}
+
+int libssh_proxy_init() {
+  if ((libssh_proxy_env = malloc(sizeof(struct _libssh_proxy_head))) == NULL) {
+    return -1;
+  }
+
+  LIST_INIT(libssh_proxy_env);
+
+  return 0;
+}
+
+int libssh_proxy_destroy() {
+  while (!LIST_EMPTY(libssh_proxy_env)) {
+    struct libssh_proxy_func* entry = LIST_FIRST(libssh_proxy_env);
+
+    while (!LIST_EMPTY(&entry->options_head)) {
+      struct libssh_proxy_option* oentry = LIST_FIRST(&entry->options_head);
+      LIST_REMOVE(oentry, entries);
+      free(oentry);
+    }
+
+    LIST_REMOVE(entry, entries);
+    free(entry);
+  }
+
+  free(libssh_proxy_env);
+
+  return 0;
+}
+
+int libssh_proxy_get_option_int(const char* func, const char* option, int def) {
+  struct libssh_proxy_func* func_entry = find_func(func, 0);
+
+  if (func_entry != NULL) {
+    struct libssh_proxy_option* option_entry;
+
+    option_entry = find_option(func_entry, option, 0);
+    return (option_entry != NULL) ? option_entry->value : def;
+  } else {
+    return def;
+  }
+}
+
+int libssh_proxy_set_option_int(const char* func, const char* option, int val) {
+  struct libssh_proxy_func* func_entry = find_func(func, 1);
+  struct libssh_proxy_option* option_entry = find_option(func_entry, option, 1);
+
+  option_entry->value = val;
+
+  return 0;
+}
---
+++ test/libssh_proxy.h
@@ -0,0 +1,15 @@
+#ifndef LIBSSH_PROXY_H
+#define LIBSSH_PROXY_H
+
+struct _libssh_proxy_head;
+typedef struct _libssh_proxy_head* libssh_proxy_t;
+
+libssh_proxy_t libssh_proxy_env;
+
+int libssh_proxy_init();
+int libssh_proxy_destroy();
+
+int libssh_proxy_get_option_int(const char* func, const char* option, int def);
+int libssh_proxy_set_option_int(const char* func, const char* option, int val);
+
+#endif  /* LIBSSH_PROXY_H */
