7f13e191342d40dbbedb09fe9f90ba947f14e038
Robin Doer <robin@robind.de>
2012-07-29 10:16:42 +0200

    Skeleton for handle_sshbind-tests

---
+++ test/libgrsd/handle_sshbind_tests.c
@@ -0,0 +1,7 @@
+#include <check.h>
+
+TCase* handle_sshbind_tcase() {
+  TCase* tc = tcase_create("handle_sshbind");
+
+  return tc;
+}
--- test/suite.c
+++ test/suite.c
@@ -4,6 +4,7 @@
 
 extern TCase* grsd_tcase();
 extern TCase* handle_pipe_tcase();
+extern TCase* handle_sshbind_tcase();
 extern TCase* process_tcase();
 extern TCase* session_tcase();
 extern TCase* slist_tcase();
@@ -13,6 +14,7 @@ static Suite* grs_suite() {
 
   suite_add_tcase(s, grsd_tcase());
   suite_add_tcase(s, handle_pipe_tcase());
+  suite_add_tcase(s, handle_sshbind_tcase());
   suite_add_tcase(s, process_tcase());
   suite_add_tcase(s, session_tcase());
   suite_add_tcase(s, slist_tcase());
