01a15bb159ef04823896885f8f70cca18df032f6
Robin Doer <robin@robind.de>
2012-08-10 18:32:46 +0200

    libssh_proxy_[get|set]_option_list()

--- test/libssh_proxy.c
+++ test/libssh_proxy.c
@@ -9,6 +9,7 @@ struct libssh_proxy_option {
 
   union {
     int int_val;
+    struct list_head* list_val;
     int* intptr_val;
   } v;
 
@@ -114,6 +115,32 @@ int libssh_proxy_set_option_int(const char* func, const char* option, int val) {
   struct libssh_proxy_option* option_entry = find_option(func_entry, option, 1);
 
   option_entry->v.int_val = val;
+
+  return 0;
+}
+
+struct list_head* libssh_proxy_get_option_list(const char* func,
+                                               const char* option,
+                                               struct list_head* def) {
+  struct libssh_proxy_func* func_entry = find_func(func, 0);
+
+  if (func_entry != NULL) {
+    struct libssh_proxy_option* option_entry;
+
+    option_entry = find_option(func_entry, option, 0);
+    return (option_entry != NULL) ? option_entry->v.list_val : def;
+  } else {
+    return def;
+  }
+}
+
+int libssh_proxy_set_option_list(const char* func,
+                                 const char* option,
+                                 struct list_head* val) {
+  struct libssh_proxy_func* func_entry = find_func(func, 1);
+  struct libssh_proxy_option* option_entry = find_option(func_entry, option, 1);
+
+  option_entry->v.list_val = val;
 
   return 0;
 }
--- test/libssh_proxy.h
+++ test/libssh_proxy.h
@@ -1,16 +1,36 @@
 #ifndef LIBSSH_PROXY_H
 #define LIBSSH_PROXY_H
 
+#include <sys/queue.h>
+
 struct _libssh_proxy_head;
 typedef struct _libssh_proxy_head* libssh_proxy_t;
 
 libssh_proxy_t libssh_proxy_env;
+
+struct list_entry {
+  union {
+    int int_val;
+  } v;
+
+  LIST_ENTRY(list_entry) entries;
+};
+
+LIST_HEAD(list_head, list_entry);
 
 int libssh_proxy_init();
 int libssh_proxy_destroy();
 
 int libssh_proxy_get_option_int(const char* func, const char* option, int def);
 int libssh_proxy_set_option_int(const char* func, const char* option, int val);
+
+struct list_head* libssh_proxy_get_option_list(const char* func,
+                                               const char* option,
+                                               struct list_head* def);
+int libssh_proxy_set_option_list(const char* func,
+                                 const char* option,
+                                 struct list_head* val);
+
 int* libssh_proxy_get_option_intptr(const char* func, const char* option, int* def);
 int libssh_proxy_set_option_intptr(const char* func, const char* option, int* val);
 
