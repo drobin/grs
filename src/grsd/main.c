0bd9bbfaadc890323c9d5be5492a0ca2e4e38f2d
Robin Doer <robin@robind.de>
2012-09-04 19:28:19 +0200

    Initialize the SSH server bind

--- src/grsd/main.c
+++ src/grsd/main.c
@@ -20,9 +20,10 @@ static void usage() {
 
 int main(int argc, char** argv) {
   struct sigaction sa;
-  char* hostkey;
-  int port;
-  int ch;
+  ssh_bind bind;
+  char* hostkey = "";
+  int port = 22;
+  int ch, result;
 
   memset(&sa, 0, sizeof(struct sigaction));
   sa.sa_handler = leave_handler;
@@ -41,6 +42,39 @@ int main(int argc, char** argv) {
       usage();
     }
   }
+
+  if ((bind = ssh_bind_new()) != NULL) {
+    log_debug("SSH server bind created");
+  } else {
+    log_err("Failed to create the SSH server bind");
+    return 1;
+  }
+
+  if ((result = ssh_bind_options_set(bind, SSH_BIND_OPTIONS_BINDPORT, &port)) == SSH_OK) {
+    log_debug("SSH server listing on %i", port);
+  } else {
+    log_err("Failed to set SSH server port: %s", ssh_get_error(bind));
+    ssh_bind_free(bind);
+    return 1;
+  }
+
+  if ((result = ssh_bind_options_set(bind, SSH_BIND_OPTIONS_RSAKEY, hostkey)) == SSH_OK) {
+    log_debug("SSH server hostkey: %s", hostkey);
+  } else {
+    log_err("Failed to set SSH server hostkey: %s", ssh_get_error(bind));
+    ssh_bind_free(bind);
+    return 1;
+  }
+
+  if (ssh_bind_listen(bind) == 0) {
+    log_debug("SSH server is listining");
+  } else {
+    log_err("Error listening to socket: %s", ssh_get_error(bind));
+    ssh_bind_free(bind);
+    return 1;
+  }
+
+  ssh_bind_free(bind);
 
   return 0;
 }
