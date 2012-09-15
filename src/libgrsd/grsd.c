7a7da588fd646eb9bfbdaa206294bf6864d96ff4
Robin Doer <robin@robind.de>
2012-09-15 17:29:59 +0200

    Cleanup of grsd-module

--- src/libgrsd/grsd.c
+++ src/libgrsd/grsd.c
@@ -1,39 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <libssh/libssh.h>
-#include <libssh/server.h>
-
 #include "grsd.h"
-#include "log.h"
 
 struct _grsd {
-  /**
-   * Instance to the SSH-server-bind.
-   */
-  ssh_bind bind;
-
-  /**
-   * Port where the SSH-server is listening.
-   */
-  unsigned int listen_port;
-
-  /**
-   * Hostkey of the SSH-server.
-   */
-  char* hostkey;
 };
-
-int grsd_bind_accept(grsd_t handle, ssh_session session) {
-  int result;
-
-  if ((result = ssh_bind_accept(handle->bind, session)) != SSH_OK) {
-    log_err("Error accepting connection: %s", ssh_get_error(handle->bind));
-  }
-
-  return result;
-}
 
 grsd_t grsd_init() {
   struct _grsd* handle;
@@ -42,15 +13,7 @@ grsd_t grsd_init() {
     return NULL;
   }
 
-  // Create the libssh-sshbind
-  if ((handle->bind = ssh_bind_new()) == NULL) {
-    free(handle);
-    return NULL;
-  }
-
-  // Some default SSH-daemon-information
-  handle->listen_port = 22;
-  handle->hostkey = NULL;
+  memset(handle, 0, sizeof(struct _grsd));
 
   return handle;
 }
@@ -60,98 +23,7 @@ int grsd_destroy(grsd_t handle) {
     return -1;
   }
 
-  ssh_bind_free(handle->bind);
-
-  free(handle->hostkey);
   free(handle);
 
   return 0;
-}
-
-int grsd_get_listen_port(grsd_t handle) {
-
-  if (handle == NULL) {
-    return -1;
-  }
-
-  return handle->listen_port;
-}
-
-int grsd_set_listen_port(grsd_t handle, unsigned int port) {
-  int result;
-
-  if (handle == NULL) {
-    return -1;
-  }
-
-  if (port < 1 || port > 65536) {
-    return -1;
-  }
-
-  result = ssh_bind_options_set(handle->bind, SSH_BIND_OPTIONS_BINDPORT, &port);
-
-  if (result == SSH_OK) {
-    handle->listen_port = port;
-    return 0;
-  } else {
-    return -1;
-  }
-}
-
-const char* grsd_get_hostkey(grsd_t handle) {
-  if (handle == NULL) {
-    return NULL;
-  }
-
-  return handle->hostkey;
-}
-
-int grsd_set_hostkey(grsd_t handle, const char* path) {
-  int result;
-
-  if (handle == NULL || path == NULL) {
-    return -1;
-  }
-
-  result = ssh_bind_options_set(handle->bind, SSH_BIND_OPTIONS_RSAKEY, path);
-
-  if (result == SSH_OK) {
-    if (handle->hostkey != NULL) {
-      free(handle->hostkey);
-    }
-
-    if (asprintf(&handle->hostkey, path) == -1) {
-      return -1;
-    }
-
-    return 0;
-  } else {
-    return -1;
-  }
-}
-
-int grsd_listen(grsd_t handle) {
-  int exit_code = 0;
-
-  log_debug("Enter grsd_listen");
-
-  if (handle == NULL) {
-    return -1;
-  }
-
-  if (ssh_bind_listen(handle->bind) < 0) {
-    log_fatal("Error listening to socket: %s\n", ssh_get_error(handle->bind));
-    return -1;
-  }
-
-  log_debug("Leaving grsd_listen with %i", exit_code);
-  return exit_code;
-}
-
-int grsd_listen_get_fd(grsd_t handle) {
-  if (handle == NULL || handle->bind == NULL) {
-    return -1;
-  }
-
-  return ssh_bind_get_fd(handle->bind);
 }
--- test/libgrsd/grsd_tests.c
+++ test/libgrsd/grsd_tests.c
@@ -8,9 +8,8 @@
 
 #include "../../src/libgrsd/grsd.h"
 
-//static grsd_t handle;
+static grsd_t handle;
 
-#if 0
 static void setup() {
   fail_unless((handle = grsd_init()) != NULL);
 }
@@ -20,145 +19,16 @@ static void teardown() {
   handle = NULL;
 }
 
-START_TEST(listen_null_handle) {
-  int result;
-
-  fail_unless((result = grsd_listen(NULL)) == -1);
+START_TEST(destroy_null_handle) {
+  fail_unless(grsd_destroy(NULL) == -1);
 }
 END_TEST
-
-START_TEST(listen_exit_null_handle) {
-  int result;
-
-  fail_unless((result = grsd_listen_exit(NULL)) == -1);
-}
-END_TEST
-
-START_TEST(listen_exit) {
-  pid_t pid = check_fork();
-
-  if (pid == 0) {
-    fail_unless(grsd_listen_exit(handle) == 0);
-  } else if (pid > 0) {
-    grsd_set_listen_port(handle, 4711);
-    fail_unless(grsd_listen(handle) == 0);
-    check_waitpid_and_exit(pid);
-  } else {
-    fail(strerror(errno));
-  }
-}
-END_TEST
-
-START_TEST(listen_connect) {
-  pid_t pid = check_fork();
-
-  if (pid == 0) {
-    ssh_session session = ssh_new();
-    ssh_channel channel;
-    char buf[512];
-    size_t nread;
-
-    ssh_options_set(session, SSH_OPTIONS_HOST, "localhost");
-    ssh_options_set(session, SSH_OPTIONS_PORT_STR, "4711");;
-
-    ssh_connect(session);
-
-    ssh_userauth_password(session, "foo", "foo");
-    channel = ssh_channel_new(session);
-    ssh_channel_open_session(channel);
-    ssh_channel_request_exec(channel, "ls");
-    while ((nread = ssh_channel_read(channel, buf, sizeof(buf), 0)) > 0);
-    ssh_channel_send_eof(channel);
-    ssh_channel_close(channel);
-    ssh_channel_free(channel);
-    ssh_disconnect(session);
-    ssh_free(session);
-    grsd_listen_exit(handle);
-  } else if (pid > 0) {
-    grsd_set_listen_port(handle, 4711);
-    grsd_set_hostkey(handle, hostkey_get_path());
-    fail_unless(grsd_listen(handle) == 0);
-    check_waitpid_and_exit(pid);
-  } else {
-    fail(strerror(errno));
-  }
-}
-END_TEST
-
-START_TEST(get_listen_port_default) {
-  fail_unless(grsd_get_listen_port(handle) == 22);
-}
-END_TEST
-
-START_TEST(get_listen_port_null_handle) {
-  fail_unless(grsd_get_listen_port(NULL) == -1);
-}
-END_TEST
-
-START_TEST(set_listen_port_null_handle) {
-  fail_unless(grsd_set_listen_port(NULL, 22) == -1);
-}
-END_TEST
-
-START_TEST(set_listen_port_out_of_range) {
-  fail_unless(grsd_set_listen_port(NULL, 0) == -1);
-  fail_unless(grsd_set_listen_port(NULL, 65537) == -1);
-}
-END_TEST
-
-START_TEST(get_set_listen_port) {
-  fail_unless(grsd_set_listen_port(handle, 4711) == 0);
-  fail_unless(grsd_get_listen_port(handle) == 4711);
-}
-END_TEST
-
-START_TEST(get_hostkey_null_handler) {
-  fail_unless(grsd_get_hostkey(NULL) == NULL);
-}
-END_TEST
-
-START_TEST(get_hostkey_default) {
-  fail_unless(grsd_get_hostkey(handle) == NULL);
-}
-END_TEST
-
-START_TEST(set_hostkey_null_handler) {
-  fail_unless(grsd_set_hostkey(NULL, "/path/to/key") == -1);
-}
-END_TEST
-
-START_TEST(set_hostkey_null_path) {
-  fail_unless(grsd_set_hostkey(handle, NULL) == -1);
-}
-END_TEST
-
-START_TEST(get_set_hostkey) {
-  fail_unless(grsd_set_hostkey(handle, "/path/to/key") == 0);
-  fail_unless(strcmp(grsd_get_hostkey(handle), "/path/to/key") == 0);
-}
-END_TEST
-#endif
 
 TCase* grsd_tcase() {
   TCase* tc = tcase_create("grsd");
-  //tcase_add_checked_fixture(tc, setup, teardown);
+  tcase_add_checked_fixture(tc, setup, teardown);
 
-#if 0
-  tcase_add_test(tc, listen_null_handle);
-  tcase_add_test(tc, listen_exit_null_handle);
-  tcase_add_test(tc, listen_exit);
-  tcase_add_test(tc, listen_connect);
-  tcase_add_test(tc, get_listen_port_null_handle);
-  tcase_add_test(tc, get_listen_port_default);
-  tcase_add_test(tc, set_listen_port_null_handle);
-  tcase_add_test(tc, set_listen_port_out_of_range);
-  tcase_add_test(tc, get_set_listen_port);
-  tcase_add_test(tc, get_hostkey_null_handler);
-  tcase_add_test(tc, get_hostkey_default);
-  tcase_add_test(tc, set_hostkey_null_handler);
-  tcase_add_test(tc, set_hostkey_null_path);
-  tcase_add_test(tc, get_set_hostkey);
-#endif
+  tcase_add_test(tc, destroy_null_handle);
 
   return tc;
 }
