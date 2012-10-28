93e6df1629edff0bcfa9fdec7a04ec33c0882e9f
Robin Doer <robin@robind.de>
2012-10-28 18:29:25 +0100

    Implementation of packfile_transfer()

--- src/extension/git/packfile_transfer.c
+++ src/extension/git/packfile_transfer.c
@@ -17,13 +17,107 @@
  *
  ******************************************************************************/
 
+#include <arpa/inet.h>
+
+#include <libgrs/log.h>
+
+#include "checksum.h"
+#include "compress.h"
 #include "protocol.h"
+
+static void objectsbuf_destroy(binbuf_t objects) {
+  int idx;
+
+  for (idx = 0; idx < binbuf_get_size(objects); idx++) {
+    const struct packfile_object* obj = binbuf_get(objects, idx);
+    buffer_destroy(obj->content);
+  }
+
+  binbuf_destroy(objects);
+}
+
+static int write_packfile_header(int nobjects, buffer_t out) {
+  const uint32_t num = htonl(nobjects);
+  unsigned char hdr[] = {
+    'P', 'A', 'C', 'K',
+    0x00, 0x00, 0x00, 0x02
+  };
+
+  return buffer_append(out, hdr, sizeof(hdr)) +
+         buffer_append(out, &num, sizeof(num));
+}
+
+static int write_packfile_object_header(const struct packfile_object* object,
+                                        buffer_t out) {
+  uint8_t hdr_byte;
+  size_t size = buffer_get_size(object->content);
+
+  hdr_byte = (object->type & 0x7) << 4;
+  hdr_byte |= size & 0xf;
+  size >>= 4;
+
+  while (size > 0) {
+    hdr_byte |= 0x80;
+    buffer_append(out, &hdr_byte, 1);
+
+    hdr_byte = size & 0x7f;
+    size >>= 7;
+  }
+
+  buffer_append(out, &hdr_byte, 1);
+
+  return 0;
+}
+
+static int encode_packfile_object(const struct packfile_object* object,
+                                  buffer_t out) {
+  // Each packfile-object starts with a header, followed by the compressed
+  // object-content
+  return write_packfile_object_header(object, out) +
+         buffer_compress(object->content, out);
+}
 
 int packfile_transfer(const char* repository, binbuf_t commits,
                       packfile_objects_cb obj_cb, buffer_t out) {
+  binbuf_t objects;
+  unsigned char sha[20];
+  int idx;
+
   if (repository == NULL || commits == NULL || obj_cb == NULL || out == NULL) {
     return -1;
   }
+
+  // Fetch packfile-objects from callback
+  objects = binbuf_create(sizeof(struct packfile_object));
+  if (obj_cb(repository, commits, objects) != 0) {
+    objectsbuf_destroy(objects);
+    return -1;
+  }
+
+  // Write global packfile-header
+  if (write_packfile_header(binbuf_get_size(objects), out) != 0) {
+    objectsbuf_destroy(objects);
+    return -1;
+  }
+
+  // Encode packfile-objects and assign to output-buffer
+  for (idx = 0; idx < binbuf_get_size(objects); idx++) {
+    const struct packfile_object* object = binbuf_get(objects, idx);
+    if (encode_packfile_object(object, out) != 0) {
+      objectsbuf_destroy(objects);
+      return -1;
+    }
+  }
+
+  // Finally append the SHA1-checksum to the output-buffer
+  if (buffer_checksum(out, sha) == 0) {
+    buffer_append(out, sha, sizeof(sha));
+  } else {
+    objectsbuf_destroy(objects);
+    return -1;
+  }
+
+  objectsbuf_destroy(objects);
 
   return 0;
 }
--- test/ext_git/packfile_transfer_tests.c
+++ test/ext_git/packfile_transfer_tests.c
@@ -21,13 +21,59 @@
 
 #include "../../src/extension/git/protocol.h"
 
-int packfile_objects_stub(const char* repository, binbuf_t commits,
-                          binbuf_t objects) {
+static binbuf_t commits;
+static buffer_t out;
+
+static int empty_stub(const char* repository, binbuf_t c, binbuf_t o) {
+  fail_unless(strcmp(repository, "xxx") == 0);
+  fail_unless(c == commits);
+  fail_unless(binbuf_get_size(o) == 0);
+
   return 0;
 }
 
-static binbuf_t commits;
-static buffer_t out;
+static int one_stub(const char* repository, binbuf_t c, binbuf_t o) {
+  struct packfile_object* obj;
+
+  fail_unless(strcmp(repository, "xxx") == 0);
+  fail_unless(c == commits);
+  fail_unless(binbuf_get_size(o) == 0);
+
+  obj = binbuf_add(o);
+  obj->type = 1;
+  obj->content = buffer_create();
+  buffer_append(obj->content, "123", 3);
+
+  return 0;
+}
+
+static int two_stub(const char* repository, binbuf_t c, binbuf_t o) {
+  struct packfile_object* obj;
+
+  fail_unless(strcmp(repository, "xxx") == 0);
+  fail_unless(c == commits);
+  fail_unless(binbuf_get_size(o) == 0);
+
+  obj = binbuf_add(o);
+  obj->type = 1;
+  obj->content = buffer_create();
+  buffer_append(obj->content, "123", 3);
+
+  obj = binbuf_add(o);
+  obj->type = 2;
+  obj->content = buffer_create();
+  buffer_append(obj->content, "456", 3);
+
+  return 0;
+}
+
+static int failed_stub(const char* repository, binbuf_t c, binbuf_t o) {
+  fail_unless(strcmp(repository, "xxx") == 0);
+  fail_unless(c == commits);
+  fail_unless(binbuf_get_size(o) == 0);
+
+  return -1;
+}
 
 static void setup() {
   fail_unless((commits = binbuf_create(41)) != NULL);
@@ -40,13 +86,12 @@ static void teardown() {
 }
 
 START_TEST(null_repository) {
-  fail_unless(
-    packfile_transfer(NULL, commits, packfile_objects_stub, out) == -1);
+  fail_unless(packfile_transfer(NULL, commits, empty_stub, out) == -1);
 }
 END_TEST
 
 START_TEST(null_commits) {
-  fail_unless(packfile_transfer("xxx", NULL, packfile_objects_stub, out) == -1);
+  fail_unless(packfile_transfer("xxx", NULL, empty_stub, out) == -1);
 }
 END_TEST
 
@@ -56,8 +101,58 @@ START_TEST(null_callback) {
 END_TEST
 
 START_TEST(null_out) {
-  fail_unless(
-    packfile_transfer("xxx", commits, packfile_objects_stub, NULL) == -1);
+  fail_unless(packfile_transfer("xxx", commits, empty_stub, NULL) == -1);
+}
+END_TEST
+
+START_TEST(failed_callback) {
+  fail_unless(packfile_transfer("xxx", commits, failed_stub, out) == -1);
+  fail_unless(buffer_get_size(out) == 0);
+}
+END_TEST
+
+START_TEST(no_objects) {
+  unsigned char expected_out[] = {
+    0x50, 0x41, 0x43, 0x4b, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+    0x00, 0x00, 0x02, 0x9d, 0x08, 0x82, 0x3b, 0xd8, 0xa8, 0xea,
+    0xb5, 0x10, 0xad, 0x6a, 0xc7, 0x5c, 0x82, 0x3c, 0xfd, 0x3e,
+    0xd3, 0x1e
+  };
+
+  fail_unless(packfile_transfer("xxx", commits, empty_stub, out) == 0);
+  fail_unless(buffer_get_size(out) == 32);
+  fail_unless(memcmp(buffer_get_data(out), expected_out, 32) == 0);
+}
+END_TEST
+
+START_TEST(one_object) {
+  unsigned char expected_out[] = {
+    0x50, 0x41, 0x43, 0x4b, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+    0x00, 0x01, 0x13, 0x78, 0x9c, 0x33, 0x34, 0x32, 0x06, 0x00,
+    0x01, 0x2d, 0x00, 0x97, 0x07, 0x77, 0x8d, 0x37, 0xa2, 0x12,
+    0x33, 0xad, 0xe4, 0x78, 0xfb, 0x0e, 0xa1, 0x9e, 0x52, 0x35,
+    0x10, 0xc3, 0x35, 0x2b
+  };
+
+  fail_unless(packfile_transfer("xxx", commits, one_stub, out) == 0);
+  fail_unless(buffer_get_size(out) == 44);
+  fail_unless(memcmp(buffer_get_data(out), expected_out, 44) == 0);
+}
+END_TEST
+
+START_TEST(two_objects) {
+  unsigned char expected_out[] = {
+    0x50, 0x41, 0x43, 0x4b, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+    0x00, 0x02, 0x13, 0x78, 0x9c, 0x33, 0x34, 0x32, 0x06, 0x00,
+    0x01, 0x2d, 0x00, 0x97, 0x23, 0x78, 0x9c, 0x33, 0x31, 0x35,
+    0x03, 0x00, 0x01, 0x3f, 0x00, 0xa0, 0x9b, 0x5b, 0x1b, 0xe5,
+    0xab, 0x9f, 0x00, 0x63, 0x3b, 0xd5, 0xbe, 0x8b, 0x19, 0xa6,
+    0x06, 0x28, 0x07, 0xfd, 0x58, 0x0b
+  };
+
+  fail_unless(packfile_transfer("xxx", commits, two_stub, out) == 0);
+  fail_unless(buffer_get_size(out) == 56);
+  fail_unless(memcmp(buffer_get_data(out), expected_out, 56) == 0);
 }
 END_TEST
 
@@ -69,6 +164,10 @@ TCase* packfile_transfer_tcase() {
   tcase_add_test(tc, null_commits);
   tcase_add_test(tc, null_callback);
   tcase_add_test(tc, null_out);
+  tcase_add_test(tc, failed_callback);
+  tcase_add_test(tc, no_objects);
+  tcase_add_test(tc, one_object);
+  tcase_add_test(tc, two_objects);
 
   return tc;
 }
